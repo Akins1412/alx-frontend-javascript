16e7c30cf27ce97add9912e27b89e7f330172183
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-24 19:21:39 +0000

    initials

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then((reslove) => ({ status: 200, body: "Success" }))
+    .catch((err) => Error())
+    .finally(() => {
+      console.log("Got a response from the API");
+    });
+}
