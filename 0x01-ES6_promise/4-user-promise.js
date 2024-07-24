7891f144769887ef87370af7789456bd465a09e7
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-24 20:46:46 +0000

    initials

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function signUpUser(firstName, lastName) {
+  return new promise((reslove) =>
+    resolve({
+      firstName,
+      lastName,
+    })
+  );
+}
