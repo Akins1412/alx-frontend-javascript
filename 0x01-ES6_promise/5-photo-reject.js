2c623955c0cc12cfe9ca65312161f9a9f6f5a293
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-24 20:55:24 +0000

    initials

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
     reject(new Error("${fileName} cannot be processed"));
