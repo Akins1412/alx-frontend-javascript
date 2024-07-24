54c64d6af11908e29ef17cbaf0da6fef24436748
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-24 21:20:23 +0000

    initials

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  let firstname = signUpUser(firstName, lastName);
-  let file = uploadPhoto(fileName);
+  const user = signUpUser(firstName, lastName);
+  const file = uploadPhoto(fileName);
 
   return Promise.allSettled([user, file]).then((result) =>
     result.map((obj) => ({
