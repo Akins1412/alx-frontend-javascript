05a0e6286db2195aa8a29d7e275f60fc8c502424
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-24 19:23:39 +0000

    initials

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,7 +4,5 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then((reslove) => ({ status: 200, body: "Success" }))
     .catch((err) => Error())
-    .finally(() => {
-      console.log("Got a response from the API");
-    });
+    .finally(console.log("Got a response from the API"));
 }
