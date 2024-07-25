2f602b0ed592fcce1a228e624ab508caa4def098
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-25 18:13:37 +0000

    initial

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,8 @@
 import ClassRoom from "./0-classroom.js";
 
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
 export default function initializeRooms() {
   let room = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
   return room;
