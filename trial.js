60aa0c5090c2fd040749ecbe87a5966cf117b1e1
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-25 17:44:13 +0000

    initial

---
+++ trial.js
@@ -0,0 +1,15 @@
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// let text = " ";
+// for (let X of cars) {
+//   text += X + "\n";
+// }
+
+// console.log(text);
+
+let language = "JavaScript";
+let text = "";
+for (let X of language) {
+  text += X + "\n";
+}
+console.log(text);
