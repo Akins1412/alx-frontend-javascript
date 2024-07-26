988b8c1210517b5c2573c5a2bd1ace7cd7d0fb86
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-26 02:26:51 +0000

    initial

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from "./3-currency.js";
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== "number") {
+      throw new TypeError("Amount must be number");
+    } else {
+      this._amount = amount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency_name} (${this.currency_code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
