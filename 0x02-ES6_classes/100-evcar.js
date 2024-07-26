460ea306e9015e0251e2a7943548d6fc3736c5f5
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-26 02:47:31 +0000

    initial

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,24 +1,12 @@
 import Car from "./10-car";
-/* eslint-disable no-unused-vars */
-/* eslint-disable */
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
