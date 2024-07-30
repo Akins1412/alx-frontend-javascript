08ca441419ad99287ed9d19e128ca0175a1387e6
Akins1412 <138694714+Akins1412@users.noreply.github.com>
2024-07-30 17:12:49 +0000

    initial

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+/*eslint-disable*/
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+
+  const ids = array.map((elem) => elem.id);
+  return ids;
+}
